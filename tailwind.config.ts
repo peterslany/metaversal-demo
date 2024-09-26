22af9929ff967b55a2f2545c742d91c2aeed0ad3
Peter <peter@interlay.io>
2024-09-26 15:56:08 +0200

    feat: configure color palette and typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,32 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: {
+          DEFAULT: "var(--color-background)",
+          dark: "var(--color-background-dark)",
+        },
+        foreground: "var(--color-foreground)",
+        primary: {
+          DEFAULT: "var(--color-primary)",
+          light: "var(--color-primary-light)",
+          dark: "var(--color-primary-dark)",
+        },
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+      },
+      fontFamily: {
+        heading: [
+          "var(--font-roboto-flex)",
+          {
+            fontVariationSettings: '"wght" 800, "wdth" 150',
+          },
+        ],
+        regular: [
+          "var(--font-roboto-flex)",
+          {
+            fontVariationSettings: '"wght" 500',
+          },
+        ],
       },
     },
   },
